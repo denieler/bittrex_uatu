fdfc2539e730da64dc0e25986f706499e8148248
Daniel Ostapenko <danielostapenko@gmail.com>
2017-10-21 00:16:10 +0200

    feat: add react, add clear for notifications

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,66 +1,177 @@
-(function () {
-    const NOTIFICATION_CONDITIONS_STORAGE_KEY = 'notificationConditions'
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import uuid from 'uuid/v1'
 
-    function onSave () {
-        const currencySymbol = document.getElementById('symbol').value
-        const amount = document.getElementById('amount').value
-        const condition = document.getElementById('conditions-dropdown').value
+const NOTIFICATION_CONDITIONS_STORAGE_KEY = 'notificationConditions'
+const LESS_OR_EQUAL = '<='
 
+class NotificationConditionsList extends Component {
+    onClearConditions () {
+        this.props.clearConditions()
+    }
+
+    render () {
+        return (
+            <div className='notifications-list'>
+                <div className='notifications-list__list'>
+                {
+                    this.props.conditionsList 
+                    ? this.props.conditionsList.map(
+                        c => (
+                            <div style={c.off ? {} : {color: 'green'}} key={uuid()}>
+                                {c.currency} {c.condition} {c.value}
+                            </div>
+                        )
+                    )
+                    : null
+                }
+                </div>
+
+                <div className='notifications-list__clear-button'>
+                    <button onClick={_ => this.onClearConditions()}>Clear All</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+class AddCondition extends Component {
+
+    constructor () {
+        super()
+        this.state = {
+            currency: null,
+            condition: LESS_OR_EQUAL,
+            amount: null
+        }
+    }
+
+    onCurrencyChange (e) {
+        this.setState({
+            currency: e.target.value
+        })
+    }
+
+    onConditionChange (e) {
+        this.setState({
+            condition: e.target.value
+        })
+    }
+
+    onAmountChange (e) {
+        this.setState({
+            amount: e.target.value
+        })
+    }
+
+    onSave () {
         const notificationCondition = {
-            currency: currencySymbol,
-            condition: condition,
-            value: parseFloat(amount),
+            currency: this.state.currency,
+            condition: this.state.condition,
+            value: parseFloat(this.state.amount),
             off: false
         }
-
+    
         chrome.storage.local.get(NOTIFICATION_CONDITIONS_STORAGE_KEY, ({notificationConditions}) => {
             chrome.storage.local.set({
                 [NOTIFICATION_CONDITIONS_STORAGE_KEY]: notificationConditions.concat(notificationCondition)
             })
         })
+
+        this.props.onAddedNewCondition(notificationCondition)
     }
 
-    function loadExistingNotificationConditions () {
-        const notificationConditionsDiv = document.getElementById('notification-conditions')
+    render () {
+        return (
+            <div className='add-condition'>
+                <div className='add-condition__column'>
+                    <label>Currency</label>
+                    <input onChange={e => this.onCurrencyChange(e)} />
+                </div>
 
-        chrome.storage.local.get(NOTIFICATION_CONDITIONS_STORAGE_KEY, ({notificationConditions}) => {
-            notificationConditions.forEach(c => {
-                var node = document.createElement('DIV')
-                var textnode = document.createTextNode(c.currency + ' ' + c.condition + ' ' + c.value)
-                node.appendChild(textnode)
+                <div className='add-condition__column'>
+                    <label>Condition</label>
+                    <select defaultValue={LESS_OR_EQUAL} onChange={e => this.onConditionChange(e)}>
+                        <option value={LESS_OR_EQUAL}>{LESS_OR_EQUAL}</option>
+                        <option value='>='>{'>='}</option>
+                        <option value='<'>{'<'}</option>
+                        <option value='>'>{'>'}</option>
+                    </select>
+                </div>
 
-                if (!c.off) {
-                    node.style.color = 'green'
-                }
+                <div className='add-condition__column'>
+                    <label>Amount</label>
+                    <input onChange={e => this.onAmountChange(e)} />
+                </div>
 
-                notificationConditionsDiv.appendChild(node) 
+                <div className='add-condition__column'>
+                    <button className='add-condition__column__save-button' onClick={e => this.onSave(e)}>Save</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+class App extends Component {
+    constructor () {
+        super()
+        this.state = {
+            conditionsList: []
+        }
+    }
+
+    componentDidMount () {
+        this.loadExistingNotificationConditions()
+        .then(notificationConditions => {
+            this.setState({
+                conditionsList: notificationConditions
             })
         })
     }
 
-    function initDropdownWithConditions () {
-        const dropdown = document.getElementById('conditions-dropdown')
-
-        const conditions = [
-            '<=',
-            '>=',
-            '<',
-            '>'
-        ]
-
-        conditions.forEach(c => {
-            var node = document.createElement('OPTION')
-            node.value = c
-            var textnode = document.createTextNode(c)
-            node.appendChild(textnode)
-            dropdown.appendChild(node)
+    loadExistingNotificationConditions () {
+        const notificationConditionsDiv = document.getElementById('notification-conditions')
+    
+        return new Promise((resolve, reject) => {
+            chrome.storage.local.get(
+                NOTIFICATION_CONDITIONS_STORAGE_KEY, 
+                ({notificationConditions}) => resolve(notificationConditions)
+            )
         })
     }
 
-    document.addEventListener('DOMContentLoaded', _ => {
-        loadExistingNotificationConditions()
+    onAddCondition (condition) {
+        this.state.conditionsList.push(condition)
+        this.setState({
+            conditionsList: this.state.conditionsList
+        })
+    }
 
-        document.getElementById('save-button').addEventListener('click', onSave)
-        initDropdownWithConditions()
-    })
-})()
+    clearConditions () {
+        chrome.storage.local.set({
+            [NOTIFICATION_CONDITIONS_STORAGE_KEY]: []
+        })
+        this.setState({
+            conditionsList: []
+        })
+    }
+
+    render () {
+        return (
+            <div>
+                <h1>Bittrex Notifications</h1>
+
+                <NotificationConditionsList 
+                    conditionsList={this.state.conditionsList} 
+                    clearConditions={_ => this.clearConditions()} />
+
+                <AddCondition 
+                    onAddedNewCondition={condition => this.onAddCondition(condition)} />
+            </div>
+        )
+    }
+}
+
+document.addEventListener('DOMContentLoaded', _ => {
+    ReactDOM.render(<App />, document.getElementById('app'))
+})
