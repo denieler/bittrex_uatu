92d9e22c3f4d1ea8d270b53a7a2a98ad36185970
Daniel Ostapenko <danielostapenko@gmail.com>
2017-10-20 21:47:14 +0200

    feat: add condition to popup

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,43 +1,66 @@
-const NOTIFICATION_CONDITIONS_STORAGE_KEY = 'notificationConditions'
+(function () {
+    const NOTIFICATION_CONDITIONS_STORAGE_KEY = 'notificationConditions'
 
-function onSave () {
-    const currencySymbol = document.getElementById('symbol').value
-    const lessThan = document.getElementById('less-than').value
+    function onSave () {
+        const currencySymbol = document.getElementById('symbol').value
+        const amount = document.getElementById('amount').value
+        const condition = document.getElementById('conditions-dropdown').value
 
-    const notificationCondition = {
-        currency: currencySymbol,
-        condition: '<=',
-        value: parseFloat(lessThan),
-        off: false
+        const notificationCondition = {
+            currency: currencySymbol,
+            condition: condition,
+            value: parseFloat(amount),
+            off: false
+        }
+
+        chrome.storage.local.get(NOTIFICATION_CONDITIONS_STORAGE_KEY, ({notificationConditions}) => {
+            chrome.storage.local.set({
+                [NOTIFICATION_CONDITIONS_STORAGE_KEY]: notificationConditions.concat(notificationCondition)
+            })
+        })
     }
 
-    chrome.storage.local.get(NOTIFICATION_CONDITIONS_STORAGE_KEY, ({notificationConditions}) => {
-        chrome.storage.local.set({
-            [NOTIFICATION_CONDITIONS_STORAGE_KEY]: notificationConditions.concat(notificationCondition)
+    function loadExistingNotificationConditions () {
+        const notificationConditionsDiv = document.getElementById('notification-conditions')
+
+        chrome.storage.local.get(NOTIFICATION_CONDITIONS_STORAGE_KEY, ({notificationConditions}) => {
+            notificationConditions.forEach(c => {
+                var node = document.createElement('DIV')
+                var textnode = document.createTextNode(c.currency + ' ' + c.condition + ' ' + c.value)
+                node.appendChild(textnode)
+
+                if (!c.off) {
+                    node.style.color = 'green'
+                }
+
+                notificationConditionsDiv.appendChild(node) 
+            })
         })
-    })
-}
+    }
 
-function loadExistingNotificationConditions () {
-    const notificationConditionsDiv = document.getElementById('notification-conditions')
+    function initDropdownWithConditions () {
+        const dropdown = document.getElementById('conditions-dropdown')
 
-    chrome.storage.local.get(NOTIFICATION_CONDITIONS_STORAGE_KEY, ({notificationConditions}) => {
-        notificationConditions.forEach(c => {
-            var node = document.createElement('DIV')
-            var textnode = document.createTextNode(c.currency + ' ' + c.condition + ' ' + c.value)
+        const conditions = [
+            '<=',
+            '>=',
+            '<',
+            '>'
+        ]
+
+        conditions.forEach(c => {
+            var node = document.createElement('OPTION')
+            node.value = c
+            var textnode = document.createTextNode(c)
             node.appendChild(textnode)
-
-            if (!c.off) {
-                node.style.color = 'green'
-            }
-
-            notificationConditionsDiv.appendChild(node) 
+            dropdown.appendChild(node)
         })
+    }
+
+    document.addEventListener('DOMContentLoaded', _ => {
+        loadExistingNotificationConditions()
+
+        document.getElementById('save-button').addEventListener('click', onSave)
+        initDropdownWithConditions()
     })
-}
-
-document.addEventListener('DOMContentLoaded', _ => {
-    loadExistingNotificationConditions()
-
-    document.getElementById('save-button').addEventListener('click', onSave)
-})
+})()
